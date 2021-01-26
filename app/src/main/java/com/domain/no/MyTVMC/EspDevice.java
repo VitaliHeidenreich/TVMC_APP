3acdf686324c06cbc330361feb0278967f0c035c
Vitaliy H <vitaliy.nba@googlemail.com>
2021-01-26 23:30:22 +0100

    Erweiterung Teil 1

---
+++ app/src/main/java/com/domain/no/MyTVMC/EspDevice.java
@@ -0,0 +1,54 @@
+package com.domain.no.MyTVMC;
+
+public class EspDevice {
+
+    EspDevice(){
+        // nop
+    }
+
+    EspDevice(String name, String adresse){
+        Name = name;
+        Address = adresse;
+    }
+
+    private String Name;
+    private String Address;
+    //Dif connection address
+    private static String ConName = null;
+    private static String ConAddress = null;
+    private static final String defDev = "MyTV_";
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getAdresse() {
+        return Address;
+    }
+
+    public void setAdresse(String adresse) {
+        Address = adresse;
+    }
+
+    public String getConAddress(){
+        return ConAddress;
+    }
+
+    public void setConAddress(String conAddress) {
+        ConAddress = conAddress;
+    }
+
+    public String getConName() {
+        return ConName;
+    }
+
+    public void setConName(String conName){
+        ConName = conName;
+    }
+
+    public static final String getDefDev(){ return defDev; }
+}
---
+++ app/src/main/java/com/domain/no/MyTVMC/SettingsDialog.java
@@ -0,0 +1,84 @@
+package com.domain.no.MyTVMC;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatDialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+/**
+ * Created by vitali on 19.03.2018.
+ */
+
+public class SettingsDialog extends AppCompatDialogFragment
+{
+    private EditText editConnectivity;
+    private CheckBox cbCollisionReset;
+    //private Spinner spinnerUebergang;
+    private ExampleDialogListener listener;
+    MainActivity mMain = new MainActivity();
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder((getActivity()));
+
+        LayoutInflater mInflater = getActivity().getLayoutInflater();
+        View mView = mInflater.inflate(R.layout.layout_settings, null);
+
+        editConnectivity = mView.findViewById(R.id.editSerialText);
+        cbCollisionReset = mView.findViewById(R.id.cbCollisionReset);
+        //spinnerUebergang =  mView.findViewById(R.id.spinnerUebergang);
+        //ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(mView.getContext(),R.array.Übergänge, android.R.layout.simple_spinner_item);
+        //adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //spinnerUebergang.setAdapter(adapter);
+
+        //Hole Einstellungen aus der Main
+        MainActivity mMain = new MainActivity();
+        editConnectivity.setText(mMain.getSetting());
+
+        mBuilder.setView(mView)
+                .setTitle("Settings")
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                })
+                .setPositiveButton("Okay", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        boolean iRet = false;
+                        String textToBeSended = editConnectivity.getText().toString();
+                        if( cbCollisionReset.isChecked() )
+                        {
+                            iRet = true;
+                        }
+                        listener.applyTexts(textToBeSended, iRet);
+                    }
+                });
+        //sendMessageText = getView().findViewById(R.id.edit_ip);
+        return mBuilder.create();
+    }
+
+
+    @Override
+    public void onAttach(Context context){
+        super.onAttach(context);
+        try{
+            listener = (ExampleDialogListener) context;
+        }
+        catch(ClassCastException e)
+        {
+            throw new ClassCastException(context.toString()+"bla");
+        }
+    }
+
+    public interface ExampleDialogListener{
+        void applyTexts(String textToBeSended, boolean boo);
+    }
+}
